9564b333ea7f8682ed8f2d2e92425b6559f32978
Leon Lau <leondotlau@gmail.com>
2020-07-03 11:39:26 +1000

    image piece config

--- lib/providers/game_State_Provider.dart
+++ lib/providers/game_State_Provider.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
+import '../data/image_piece_config.dart';
 
 class GameStateProvider with ChangeNotifier {
   static bool _puzzleComplete = false;
@@ -26,223 +27,6 @@ class GameStateProvider with ChangeNotifier {
   int get getTotalGridSize => _totalGridSize;
   int get getBlankSquare => _blankSquare;
   int get getGridSideSize => _gridSideSize;
-
-  // First index is the blank square, mapped to the moveable pieces allowed directions.
-  Map<int, Map<int, String>> draggableMatrix = {
-    1: {
-      2: "left",
-      3: "left",
-      4: "left",
-      5: "up",
-      9: "up",
-      13: "up",
-    },
-    2: {
-      1: "right",
-      3: "left",
-      4: "left",
-      6: "up",
-      10: "up",
-      14: "up",
-    },
-    3: {
-      1: "right",
-      2: "right",
-      4: "left",
-      7: "up",
-      11: "up",
-      15: "up",
-    },
-    4: {
-      1: "right",
-      2: "right",
-      3: "right",
-      8: "up",
-      12: "up",
-      16: "up",
-    },
-    5: {
-      1: "down",
-      6: "left",
-      7: "left",
-      8: "left",
-      9: "up",
-      13: "up",
-    },
-    6: {
-      2: "down",
-      5: "right",
-      7: "left",
-      8: "left",
-      10: "up",
-      14: "up",
-    },
-    7: {
-      3: "down",
-      5: "right",
-      6: "right",
-      8: "left",
-      11: "up",
-      15: "up",
-    },
-    8: {
-      4: "down",
-      5: "right",
-      6: "right",
-      7: "right",
-      12: "up",
-      16: "up",
-    },
-    9: {
-      1: "down",
-      5: "down",
-      10: "left",
-      11: "left",
-      12: "left",
-      13: "up",
-    },
-    10: {
-      2: "down",
-      6: "down",
-      9: "right",
-      11: "left",
-      12: "left",
-      14: "up",
-    },
-    11: {
-      3: "down",
-      7: "down",
-      9: "right",
-      10: "right",
-      12: "left",
-      15: "up",
-    },
-    12: {
-      4: "down",
-      8: "down",
-      9: "right",
-      10: "right",
-      11: "right",
-      16: "up",
-    },
-    13: {
-      1: "down",
-      5: "down",
-      9: "down",
-      14: "left",
-      15: "left",
-      16: "left",
-    },
-    14: {
-      2: "down",
-      6: "down",
-      10: "down",
-      13: "right",
-      15: "left",
-      16: "left",
-    },
-    15: {
-      3: "down",
-      7: "down",
-      11: "down",
-      13: "right",
-      14: "right",
-      16: "left",
-    },
-    16: {
-      4: "down",
-      8: "down",
-      12: "down",
-      13: "right",
-      14: "right",
-      15: "right",
-    },
-  };
-
-  // First index is the blank square, mapped to the moveable pieces
-  // and the adjacent pieces that need to move with it.
-  Map<int, Map<int, List<int>>> draggableMatrixMulti = {
-    1: {
-      3: [2],
-      4: [3, 2],
-      9: [5],
-      13: [9, 5],
-    },
-    2: {
-      4: [3],
-      10: [6],
-      14: [10, 6],
-    },
-    3: {
-      1: [2],
-      11: [7],
-      15: [11, 7],
-    },
-    4: {
-      1: [2, 3],
-      2: [3],
-      12: [8],
-      16: [12, 8],
-    },
-    5: {
-      7: [6],
-      8: [7, 6],
-      13: [9],
-    },
-    6: {
-      8: [7],
-      14: [10],
-    },
-    7: {
-      5: [6],
-      15: [11],
-    },
-    8: {
-      5: [6, 7],
-      6: [7],
-      16: [12],
-    },
-    9: {
-      1: [5],
-      11: [10],
-      12: [11, 10],
-    },
-    10: {
-      2: [6],
-      12: [11],
-    },
-    11: {
-      3: [7],
-      9: [10],
-    },
-    12: {
-      4: [8],
-      9: [10, 11],
-      10: [11]
-    },
-    13: {
-      1: [5, 9],
-      5: [9],
-      15: [14],
-      16: [15, 14],
-    },
-    14: {
-      2: [6, 10],
-      6: [10],
-      16: [15],
-    },
-    15: {
-      3: [7, 11],
-      7: [11],
-      13: [14]
-    },
-    16: {
-      4: [8, 12],
-      8: [12],
-      13: [14, 15],
-      14: [15]
-    },
-  };
 
   void setPuzzleComplete(bool complete) {
     _puzzleComplete = complete;
@@ -294,14 +78,15 @@ class GameStateProvider with ChangeNotifier {
     // Call the adjacent drag function.
     // Then we need to set its grid position to the adjacent dragged piece.
     // Otherwise this is a single piece drag so the grid position can be set to the blank square.
-    if (draggableMatrixMulti[getBlankSquare]
+    if (ImagePieceConfig.draggablePieces[getBlankSquare]
         .containsKey(piecePreviousPosition)) {
       setAdjacentPieceLeftPosition(
-          draggableMatrixMulti[getBlankSquare][piecePreviousPosition],
+          ImagePieceConfig.draggablePieces[getBlankSquare]
+              [piecePreviousPosition],
           xDistance);
 
-      pieceToUpdate['gridPosition'] =
-          draggableMatrixMulti[getBlankSquare][piecePreviousPosition][0];
+      pieceToUpdate['gridPosition'] = ImagePieceConfig
+          .draggablePieces[getBlankSquare][piecePreviousPosition][0];
 
       setBlankSquare(piecePreviousPosition);
     } else {
@@ -376,14 +161,15 @@ class GameStateProvider with ChangeNotifier {
     // Call the adjacent drag function.
     // Then we need to set its grid position to the adjacent dragged piece.
     // Otherwise this is a single piece drag so the grid position can be set to the blank square.
-    if (draggableMatrixMulti[getBlankSquare]
+    if (ImagePieceConfig.draggablePieces[getBlankSquare]
         .containsKey(piecePreviousPosition)) {
       setAdjacentPieceTopPosition(
-          draggableMatrixMulti[getBlankSquare][piecePreviousPosition],
+          ImagePieceConfig.draggablePieces[getBlankSquare]
+              [piecePreviousPosition],
           yDistance);
 
-      pieceToUpdate['gridPosition'] =
-          draggableMatrixMulti[getBlankSquare][piecePreviousPosition][0];
+      pieceToUpdate['gridPosition'] = ImagePieceConfig
+          .draggablePieces[getBlankSquare][piecePreviousPosition][0];
 
       setBlankSquare(piecePreviousPosition);
     } else {
@@ -591,7 +377,8 @@ class GameStateProvider with ChangeNotifier {
     draggableSquares.sort((a, b) => a.compareTo(b));
 
     if (draggableSquares.contains(piecePosition) &&
-        draggableMatrix[getBlankSquare][piecePosition] == direction) {
+        ImagePieceConfig.draggableDirections[getBlankSquare][piecePosition] ==
+            direction) {
       return true;
     }
 
