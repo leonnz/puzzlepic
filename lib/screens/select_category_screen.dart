addeb81529e9607905842dc367c8afecf3043c83
Leon Lau <leondotlau@gmail.com>
2020-07-14 21:23:15 +1000

    swipe to pop screen

--- lib/screens/select_category_screen.dart
+++ lib/screens/select_category_screen.dart
@@ -10,46 +10,53 @@ class SelectCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage('assets/images/background.png'),
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'Categories',
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          backgroundColor: Color(0xffffffff),
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
-            onPressed: () => Navigator.pop(context),
+    return GestureDetector(
+      onPanUpdate: (details) {
+        if (details.delta.dx > 0) {
+          Navigator.pop(context);
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/images/background.png'),
           ),
         ),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+        child: Scaffold(
+          backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'Categories',
+              style: Theme.of(context).textTheme.headline5,
             ),
-            itemCount: Images.imageList.length,
-            itemBuilder: (BuildContext context, int i) {
-              return CategoryButton(
-                categoryName: Images.imageList[i]["categoryName"],
-                categoryReadableName: Images.imageList[i]
-                    ["categoryReadableName"],
-              );
-            },
+            backgroundColor: Color(0xffffffff),
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(10),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: Images.imageList.length,
+              itemBuilder: (BuildContext context, int i) {
+                return CategoryButton(
+                  categoryName: Images.imageList[i]["categoryName"],
+                  categoryReadableName: Images.imageList[i]
+                      ["categoryReadableName"],
+                );
+              },
+            ),
           ),
         ),
       ),
--- lib/screens/select_picture_screen.dart
+++ lib/screens/select_picture_screen.dart
@@ -29,126 +29,133 @@ class _SelectPictureState extends State<SelectPicture> {
         (imageList) =>
             imageList["categoryName"] == widget.category)["categoryImages"];
 
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage('assets/images/background.png'),
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(90),
-          child: AppBar(
-            flexibleSpace: Opacity(
-              opacity: 0.8,
-              child: Image(
-                image: AssetImage(
-                    'assets/images/categories/${widget.category}_banner.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            title: Text(
-              widget.categoryReadableName,
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            backgroundColor: Color(0xffffffff),
-            iconTheme: IconThemeData(
-              color: Colors.black,
-            ),
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios),
-              onPressed: () => Navigator.pop(context),
-            ),
-            elevation: 4,
-            centerTitle: true,
-            bottom: PreferredSize(
-              child: FutureBuilder(
-                future:
-                    dbProvider.getRecordsByCategory(category: widget.category),
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<String>> snapshot) {
-                  Widget grid;
-                  if (snapshot.hasData) {
-                    grid = Padding(
-                      padding: const EdgeInsets.only(bottom: 15.0),
-                      child: Text(
-                        'Completed ${snapshot.data.length} / ${images.length}',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                    );
-                  } else {
-                    grid = Container();
-                  }
-                  return grid;
-                },
-              ),
-              preferredSize: Size.zero,
-            ),
+    return GestureDetector(
+      onPanUpdate: (details) {
+        if (details.delta.dx > 0) {
+          Navigator.pop(context);
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/images/background.png'),
           ),
         ),
-        body: FutureBuilder(
-          future: dbProvider.getRecordsByCategory(category: widget.category),
-          builder:
-              (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-            Widget grid;
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
-                grid = Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        child: GridView.builder(
-                          shrinkWrap: true,
-                          itemCount: images.length,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10,
-                          ),
-                          itemBuilder: (BuildContext context, int i) {
-                            return GestureDetector(
-                              onTap: () async {
-                                final result = await Navigator.push(
-                                  context,
-                                  CupertinoPageRoute(
-                                    builder: (context) => PuzzleScreen(
-                                      category: widget.category,
-                                      assetName: images[i]["assetName"],
-                                      readableName: images[i]["readableName"],
+        child: Scaffold(
+          backgroundColor: Color.fromRGBO(255, 255, 255, 0.7),
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(90),
+            child: AppBar(
+              flexibleSpace: Opacity(
+                opacity: 0.8,
+                child: Image(
+                  image: AssetImage(
+                      'assets/images/categories/${widget.category}_banner.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              title: Text(
+                widget.categoryReadableName,
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              backgroundColor: Color(0xffffffff),
+              iconTheme: IconThemeData(
+                color: Colors.black,
+              ),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back_ios),
+                onPressed: () => Navigator.pop(context),
+              ),
+              elevation: 4,
+              centerTitle: true,
+              bottom: PreferredSize(
+                child: FutureBuilder(
+                  future: dbProvider.getRecordsByCategory(
+                      category: widget.category),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<String>> snapshot) {
+                    Widget grid;
+                    if (snapshot.hasData) {
+                      grid = Padding(
+                        padding: const EdgeInsets.only(bottom: 15.0),
+                        child: Text(
+                          'Completed ${snapshot.data.length} / ${images.length}',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                      );
+                    } else {
+                      grid = Container();
+                    }
+                    return grid;
+                  },
+                ),
+                preferredSize: Size.zero,
+              ),
+            ),
+          ),
+          body: FutureBuilder(
+            future: dbProvider.getRecordsByCategory(category: widget.category),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+              Widget grid;
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  grid = Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: GridView.builder(
+                            shrinkWrap: true,
+                            itemCount: images.length,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                            ),
+                            itemBuilder: (BuildContext context, int i) {
+                              return GestureDetector(
+                                onTap: () async {
+                                  final result = await Navigator.push(
+                                    context,
+                                    CupertinoPageRoute(
+                                      builder: (context) => PuzzleScreen(
+                                        category: widget.category,
+                                        assetName: images[i]["assetName"],
+                                        readableName: images[i]["readableName"],
+                                      ),
                                     ),
-                                  ),
-                                );
-                                // Refreshes the pictures to show complete ticks from database
-                                if (result) setState(() {});
-                              },
-                              child: ImageButton(
-                                categoryName: widget.category,
-                                assetName: images[i]["assetName"],
-                                readableName: images[i]["readableName"],
-                                complete: (snapshot.data
-                                        .contains(images[i]["readableName"]))
-                                    ? true
-                                    : false,
-                              ),
-                            );
-                          },
+                                  );
+                                  // Refreshes the pictures to show complete ticks from database
+                                  if (result) setState(() {});
+                                },
+                                child: ImageButton(
+                                  categoryName: widget.category,
+                                  assetName: images[i]["assetName"],
+                                  readableName: images[i]["readableName"],
+                                  complete: (snapshot.data
+                                          .contains(images[i]["readableName"]))
+                                      ? true
+                                      : false,
+                                ),
+                              );
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                );
+                    ],
+                  );
+                }
+              } else {
+                grid = Container();
               }
-            } else {
-              grid = Container();
-            }
-            return grid;
-          },
+              return grid;
+            },
+          ),
         ),
       ),
     );
