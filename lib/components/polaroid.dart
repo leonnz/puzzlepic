5b794f96bb3a2691e8a0522f3fc959716947a970
Leon Lau <leondotlau@gmail.com>
2020-07-23 12:33:38 +1000

    polaroid widget

--- lib/components/polaroid.dart
+++ lib/components/polaroid.dart
@@ -1,17 +1,81 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class Polaroid extends StatefulWidget {
-//   name({Key key}) : super(key: key);
+class Polaroid extends StatefulWidget {
+  Polaroid({
+    Key key,
+    this.alignment,
+    this.angle,
+    this.image,
+    this.startInterval,
+    this.beginPosition,
+    this.endPosition,
+  }) : super(key: key);
 
-//   @override
-//   _nameState createState() => _nameState();
-// }
+  final Alignment alignment;
+  final double angle;
+  final String image;
+  final double startInterval;
+  final Offset beginPosition;
+  final Offset endPosition;
 
-// class _nameState extends State<name> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//        child: child,
-//     );
-//   }
-// }
+  @override
+  _PolaroidState createState() => _PolaroidState();
+}
+
+class _PolaroidState extends State<Polaroid>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    )..forward();
+
+    _offsetAnimation = Tween<Offset>(
+      begin: widget.beginPosition,
+      end: widget.endPosition,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Interval(widget.startInterval, 1.0, curve: Curves.elasticOut),
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Align(
+        alignment: widget.alignment,
+        child: SlideTransition(
+          position: _offsetAnimation,
+          child: Transform.rotate(
+            angle: widget.angle,
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    blurRadius: 5.0,
+                    offset: Offset(0.0, 3.0),
+                  ),
+                ],
+              ),
+              // color: Colors.red,
+              // width: state.getScreenWidth * 0.7,
+              // height: state.getScreenWidth * 0.7,
+              child: Image(
+                image: AssetImage(
+                    'assets/images/polaroids/polaroid_${widget.image}.jpg'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
