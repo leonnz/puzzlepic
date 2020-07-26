7dbee4feb6a1992e9f6133adbdf2a01b58e363d1
Leon Lau <leondotlau@gmail.com>
2020-07-26 15:03:57 +1000

    sounds

--- lib/components/category_button.dart
+++ lib/components/category_button.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 import '../screens/select_picture_screen.dart';
 import '../styles/customStyles.dart';
 import '../providers/device_provider.dart';
-import 'package:provider/provider.dart';
 
 class CategoryButton extends StatelessWidget {
   const CategoryButton(
@@ -15,18 +18,25 @@ class CategoryButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     DeviceProvider deviceProvider = Provider.of<DeviceProvider>(context);
+    AudioCache categoryButtonAudio = AudioCache(prefix: 'audio/');
+    categoryButtonAudio.load('fast_click.wav');
+    categoryButtonAudio.disableLog();
 
     return Container(
       child: GestureDetector(
-        onTap: () => Navigator.push(
-          context,
-          CupertinoPageRoute(
-            builder: (context) => SelectPicture(
-              category: categoryName,
-              categoryReadableName: categoryReadableName,
+        onTap: () {
+          categoryButtonAudio.play('fast_click.wav',
+              mode: PlayerMode.LOW_LATENCY);
+          Navigator.push(
+            context,
+            CupertinoPageRoute(
+              builder: (context) => SelectPicture(
+                category: categoryName,
+                categoryReadableName: categoryReadableName,
+              ),
             ),
-          ),
-        ),
+          );
+        },
         child: Card(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
